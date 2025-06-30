217790f63d86b012f4151f8097b4858a0041c69a
krishanveersingh9528 <ks620850@gmail.com>
2025-06-30 13:35:01 +0530

    changes

--- FrontEnd/src/redux/store.js
+++ FrontEnd/src/redux/store.js
@@ -1,6 +1,6 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import authSlice from "./authSlice";
 import jobSlice from "./jobSlice";
+import authSlice from "./authslice";
 import {
     persistStore,
     persistReducer,
